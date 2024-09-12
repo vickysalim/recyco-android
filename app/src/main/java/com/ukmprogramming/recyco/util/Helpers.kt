316533cba415b0968e932d92d3167eecab66e160
rza101 <69953361+rza101@users.noreply.github.com>
2024-09-12 12:32:30 +0700

    fix time formatter

--- app/src/main/java/com/ukmprogramming/recyco/util/Helpers.kt
+++ app/src/main/java/com/ukmprogramming/recyco/util/Helpers.kt
@@ -48,15 +48,19 @@ object Helpers {
         return myFile
     }
 
-    fun formatDate(date: String): String {
-        return SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.ROOT).parse(date)?.let {
-            SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ROOT).format(it)
+    fun formatDate(date: String?): String {
+        return date?.let {
+            SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.ROOT).parse(it)?.let { parsedDate ->
+                SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ROOT).format(parsedDate)
+            }
         } ?: "-"
     }
 
-    fun formatDateNoTime(date: String): String {
-        return SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.ROOT).parse(date)?.let {
-            SimpleDateFormat("yyyy-MM-dd", Locale.ROOT).format(it)
+    fun formatDateNoTime(date: String?): String {
+        return date?.let {
+            SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ", Locale.ROOT).parse(it)?.let { parsedDate ->
+                SimpleDateFormat("yyyy-MM-dd", Locale.ROOT).format(parsedDate)
+            }
         } ?: "-"
     }
 }
